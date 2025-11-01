7c649ed4320e722dfb2a268f2db3a9e288c1e898
Luka Tatic <taticluka19@gmail.com>
2025-11-01 23:01:15 +0100

    Pozdrav, svete

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,4 +6,8 @@ public class Calculator {
     public static void main(String[] args) {
         System.out.println("Zbir: " + saberi(2, 3));
     }
+
+    public static int oduzmi(int a, int b) {
+        return a - b;
+    }
 }
