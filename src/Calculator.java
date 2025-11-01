470420bd486d7b3cc0805606898bf730adfbf009
Luka Tatic <taticluka19@gmail.com>
2025-11-01 22:57:02 +0100

    Added Calculator class

---
+++ src/Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+    public static int saberi(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Zbir: " + saberi(2, 3));
+    }
+}
